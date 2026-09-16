2af1ad9ac7f14f017c5b885ea8dece2172484332
Sudipta De <sudiptade506@gmail.com>
2026-09-16 23:57:59 +0530

    Time: 0 ms (100%), Space: 12.6 MB (35.68%) - LeetHub

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.c
@@ -0,0 +1,16 @@
+int lengthOfLongestSubstring(char* s) {
+    int last[256];
+    for(int i=0;i<256;i++) 
+        last[i]=-1;
+    int left =0;
+    int maxLength =0;
+    for(int right =0;s[right]!='\0';right++){
+        if(last[(unsigned char)s[right]]>=left)
+            left = last[(unsigned char)s[right]]+1;
+        last[(unsigned char)s[right]]=right;
+        int length= right - left +1;
+        if(length > maxLength)
+            maxLength = length;
+    }
+    return maxLength;
+}
